75d56e0585d44102c85fb70a80369fbb152087a4
muirurii <pnkanuna@gmail.com>
2022-10-08 19:48:11 +0300

    changed file structure

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require("path");
 require("dotenv").config({
-    path: path.join(__dirname, "..", ".env"),
+    path: path.join(__dirname, "", ".env"),
 });
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
